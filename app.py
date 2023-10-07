ca527c854b8b6e2ab7c968d5c2b0943f88c96054
sparta-camp <sparta-camp@example.com>
2023-10-07 14:14:26 +0900

    フォームで送ったデータを表示

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        message = request.form['message']
+        return render_template("index.html", message=message)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
